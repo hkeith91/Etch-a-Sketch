02e386a731f67252c2fc5ebb178b4ed0af585a9a
Hector <hector.keith91@gmail.com>
2022-04-19 17:44:49 -0400

    added new event listeners and removed old ones

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
 const columnsContainer = document.querySelector("#columns-container");
 const gridSize = 16;
+let mouseOver = false;
+let clicking = false;
 
-GenerateGrid();
+generateGrid();
 
-function handleMouseEnter(){
-  this.classList.add("highlighted");
-};
-function handleClick(){
-  this.classList.add("highlighted");
+function addEvents() {
+  addEventListener("mouseover", (e) => {
+    mouseOver = true;
+    console.log("mouse over = " + mouseOver);
+  });
+  addEventListener("mouseout", (e) => {
+    mouseOver = false;
+    console.log("mouse over = " + mouseOver);
+  });
+  addEventListener("mousedown", (e) => {
+    clicking = true;
+    console.log("clicking = " + clicking);
+  });
+  addEventListener("mouseup", (e) => {
+    clicking = false;
+    console.log("clicking = " + clicking);
+  });
 }
 
-function GenerateGrid() {
+// function colorizeSquare()
+
+function generateGrid() {
   for (let i = 0; i < gridSize; i++) {
     const rowContainer = document.createElement("div");
     columnsContainer.append(rowContainer);
 
     for (let j = 0; j < gridSize; j++) {
-      const gridCell = document.createElement("div");
-      gridCell.classList.add("grid-square");
+      const gridSquare = document.createElement("div");
+      gridSquare.classList.add("grid-square");
       rowContainer.classList.add("row-container");
-      rowContainer.append(gridCell);
-      gridCell.addEventListener("mouseenter", handleMouseEnter);
+      rowContainer.append(gridSquare);
+      
+      gridSquare.addEventListener("mouseover", (e) => {
+        mouseOver = true;
+        console.log("mouse over = " + mouseOver);
+      });
+      gridSquare.addEventListener("mouseout", (e) => {
+        mouseOver = false;
+        console.log("mouse over = " + mouseOver);
+      });
+      gridSquare.addEventListener("mousedown", (e) => {
+        clicking = true;
+        console.log("clicking = " + clicking);
+      });
+      gridSquare.addEventListener("mouseup", (e) => {
+        clicking = false;
+        console.log("clicking = " + clicking);
+      });
+      //gridSquare.addEventListener("mouseover", handleMouseOver);
+      // gridSquare.addEventListener("mouseenter", handleMouseEnter);
       // gridCell.addEventListener("click", handleClick);
-      gridCell.addEventListener("click", (e) =>{
-        console.log(e.target);
-      })
+      // gridSquare.addEventListener("click", (e) =>{
+      //   console.log(e.target);
+      // })
     }
   }
 }
-
-
