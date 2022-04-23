9c38135d2715999099c13720cd6e0953c3c45f58
Hector <hector.keith91@gmail.com>
2022-04-23 08:06:28 -0400

    fixed bug with const markerMode.  Added rainbow and pencil mode functions created default mode function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const columnsContainer = document.querySelector("#columns-container");
 const clearButton = document.querySelector("#clear-button");
 const gridRangeSlider = document.querySelector("#grid-size");
-// let markerMode = document.querySelector(".marker");
+const markerMode = document.getElementById("marker-mode");
 let root = document.documentElement;
 let gridSize = 16;
 let mouseOver = false;
 let clicking = false;
 
 generateGrid();
-console.log(markerMode);
+changeMarker();
 
 function generateGrid() {
   gridRangeSlider.value = gridSize;
@@ -24,53 +24,71 @@ function generateGrid() {
       rowContainer.classList.add("row-container");
       rowContainer.append(gridSquare);
 
-      //events to highlight when clicked, or clicked and dragged
-      gridSquare.addEventListener("mouseover", (e) => {
-        mouseOver = true;
-        if (clicking && mouseOver) {
-          gridSquare.classList.add("highlighted");
-        }
-      });
-      gridSquare.addEventListener("mousedown", (e) => {
-        clicking = true;
-        if (clicking && mouseOver) {
-          gridSquare.classList.add("highlighted");
-        }
-      });
-      //events to stop highlighting when not !clicking
-      gridSquare.addEventListener("mouseout", (e) => {
-        mouseOver = false;
-      });
-      gridSquare.addEventListener("mouseup", (e) => {
-        clicking = false;
-      });
+      defaultMarker(gridSquare);
     }
   }
 }
 
-function clearCanvas(){
+function clearCanvas() {
   let currentCanvas = document.querySelectorAll(".grid-square");
-  currentCanvas.forEach(square => {
-    if (square.classList.contains("highlighted")){
+  currentCanvas.forEach((square) => {
+    if (square.classList.contains("highlighted")) {
       square.classList.remove("highlighted");
     }
   });
 }
 
-function destroyGrid(){
+function destroyGrid() {
   let currentSquares = document.querySelectorAll(".grid-square");
-  currentSquares.forEach(element => {
+  currentSquares.forEach((element) => {
     element.remove();
   });
   let currentRows = document.querySelectorAll(".row-container");
-  currentRows.forEach(element => {
+  currentRows.forEach((element) => {
     element.remove();
-  })
+  });
 }
 
-function createNewGrid(){
+function createNewGrid() {
   gridSize = gridRangeSlider.value;
   destroyGrid();
   generateGrid();
-  
+}
+
+function changeMarker() {
+  markerMode.addEventListener("change", function () {
+    if (document.getElementById("default-mode").checked) defaultMarker(element);
+  });
+  if (document.getElementById("rainbow-mode")) rainbow - m;
+}
+
+function defaultMarker(element) {
+  //events to highlight when clicked, or clicked and dragged
+  element.addEventListener("mouseover", (e) => {
+    mouseOver = true;
+    if (clicking && mouseOver) {
+      element.classList.add("highlighted");
+    }
+  });
+  element.addEventListener("mousedown", (e) => {
+    clicking = true;
+    if (clicking && mouseOver) {
+      element.classList.add("highlighted");
+    }
+  });
+  //events to stop highlighting when !clicking
+  element.addEventListener("mouseout", (e) => {
+    mouseOver = false;
+  });
+  element.addEventListener("mouseup", (e) => {
+    clicking = false;
+  });
+}
+
+function rainbowMarker() {
+  alert("rainbow!");
+}
+
+function pencilMode() {
+  alert("pencil!");
 }
